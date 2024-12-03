e9766610ad2ed29ad585886d36ab1313c2d5d449
hoangthudev <hoangthudev@gmail.com>
2024-12-03 09:09:50 +0700

    first commit

---
+++ src/main/java/com/ecom/springbootfileupload/SpringBootFIleUploadApplication.java
@@ -0,0 +1,13 @@
+package com.ecom.springbootfileupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFIleUploadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootFIleUploadApplication.class, args);
+    }
+
+}
